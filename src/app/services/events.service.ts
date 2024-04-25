2d708762a23155a293d61c481834f75f5a6b1e20
Erekle Roinishvili <erekleroinishvili@gmail.com>
2024-04-25 21:09:17 +0100

    Remove `console.log`s

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ajax } from 'rxjs/ajax'
 import { CvEvent } from '../models';
-import { firstValueFrom, map, share, tap } from 'rxjs';
+import { firstValueFrom, map, share } from 'rxjs';
 import { removeHiddenEvents } from '../utils/events';
 // import { EVENTS } from '../events';
 
@@ -12,9 +12,7 @@ export class EventsService {
 
   private eventsReply = ajax.getJSON<{events: CvEvent[]}>('events.json').pipe(
     map(reply => reply.events),
-tap(events => console.log(`Events before`, events)),
     map(events => removeHiddenEvents(events)),
-tap(events => console.log(`Events after`, events)),
     share({resetOnError: true}),
   )
   events = firstValueFrom(this.eventsReply)
