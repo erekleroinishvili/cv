d4584873ffc0db89e33b05de347c7f83ab9502c7
Erekle Roinishvili <erekleroinishvili@gmail.com>
2024-05-03 22:01:20 +0100

    Using `Image Link` component

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -1,11 +1,14 @@
 import { Component, input } from '@angular/core';
 import type { Label, LabelObject } from '../../models';
 import { normalizeLabel } from '../../utils/label.utils';
+import { ImageLinkComponent } from '../image-link/image-link.component';
 
 @Component({
   selector: 'erekle-label',
   standalone: true,
-  imports: [],
+  imports: [
+    ImageLinkComponent,
+  ],
   templateUrl: './label.component.html',
   styleUrl: './label.component.scss'
 })
--- src/app/models/label.model.ts
+++ src/app/models/label.model.ts
@@ -1,15 +1,22 @@
 export interface LabelLink {
-    type?: 'link'
+    type: 'link'
     label: string
     url: string
 }
 
-export interface LabelText extends Omit<Partial<LabelLink>, 'type'> {
+export interface LabelText {
     type: 'text'
     label: string
 }
 
-export type LabelObject = LabelLink | LabelText
+export interface LabelImage {
+    type: 'image'
+    label: string
+    url: string
+    alt?: string
+}
+
+export type LabelObject = LabelLink | LabelText | LabelImage
 
 export type Label =
     (string | LabelObject) |
