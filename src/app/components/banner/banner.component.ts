197d26f2523097056af09f6da8d71b4a9235a292
Erekle Roinishvili <erekleroinishvili@gmail.com>
2024-05-05 09:37:52 +0100

    Animating image popup

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,3 +1,4 @@
+import { animate, animateChild, group, query, state, style, transition, trigger } from '@angular/animations';
 import { Component, input, model } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,6 +9,27 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     MatButtonModule,
     MatIconModule,
+  ],
+  animations: [
+    trigger('fade', [
+      state('void', style({
+        opacity: 0,
+      })),
+      transition(':enter, :leave', [
+        group([
+          animate(300),
+          query('@popup', animateChild())
+        ])
+      ])
+    ]),
+
+    trigger('popup', [
+      state('void', style({
+        transform: 'scale(0)',
+        opacity: 0,
+      })),
+      transition(':enter, :leave', animate(300))
+    ])
   ],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
