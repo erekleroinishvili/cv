8ad6a8bca612957dfdbe9f584d2103c3b98bca6a
Erekle Roinishvili <erekleroinishvili@gmail.com>
2024-05-13 09:42:37 +0100

    Allow progress inside event

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -2,12 +2,14 @@ import { Component, input } from '@angular/core';
 import type { Label, LabelObject } from '../../models';
 import { normalizeLabel } from '../../utils/label.utils';
 import { ImageLinkComponent } from '../image-link/image-link.component';
+import { ProgressComponent } from '../progress/progress.component';
 
 @Component({
   selector: 'erekle-label',
   standalone: true,
   imports: [
     ImageLinkComponent,
+    ProgressComponent,
   ],
   templateUrl: './label.component.html',
   styleUrl: './label.component.scss'
